97e1ffc0eda0aa3f6fe0da91826d4f6e7b7cc50d
Daniel Buckmaster <dan.buckmaster@gmail.com>
2014-11-01 01:00:58 +1100

    Better quit handling.

--- game/scripts/input/main.cs
+++ game/scripts/input/main.cs
@@ -2,4 +2,11 @@ new EventManager(InputEvents) {
    queue = InputEventQueue;
 };
 
+GameEvents.subscribe(InputEvents, EvtStart);
+
+function InputEvents::onEvtStart(%this) {
+   $enableDirectInput = 1;
+   activateDirectInput();
+}
+
 exec("./menus.cs");
--- game/scripts/input/menus.cs
+++ game/scripts/input/menus.cs
@@ -4,7 +4,6 @@ InputEvents.registerEvent(EvtAdvance);
 %advance = "InputEvents.postEvent(EvtAdvance);";
 MenuActionMap.bindCmd(keyboard, space, %advance, "");
 MenuActionMap.bindCmd(keyboard, enter, %advance, "");
-//MenuActionMap.bindCmd(mouse, button0, %advance, "");
 MenuActionMap.bindCmd(gamepad, btn_a, %advance, "");
 MenuActionMap.bindCmd(gamepad, dpadr, %advance, "");
 
